183a677b326a8fbc5a38a82645a49d9c438d0627
Pavel Demidovich <p.demidovich@softmax.by>
2022-06-22 00:34:43 +0300

    posts controller

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
 
-import { registerValidation } from './validations/auth.js'
-import UserModel from './models/User.js'
+import { registerValidation, loginValidation } from './validations/validations.js'
+
+import checkAuth from './utils/checkAuth.js'
+import * as UserController from './Controllers/UserController.js'
 
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.agofg.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
@@ -19,51 +18,9 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/auth/login', (req, res) => {
-    try {
-
-    } catch(err) {
-        
-    }
-})
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
-    }
- 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
- 
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash: hash
-    })
- 
-    const user = await doc.save();
-
-    const token = jwt.sign({
-        _id: user._id
-    }, 'secrete123', { expiresIn: '30d' })
-
-    const { passwordHash, ...userData } = user._doc;
- 
-    res.json({ ...userData, token })
-
-  } catch(err) {
-    console.log(err);
-
-    res.status(500).json({
-        message: 'Не удалось зарегестрироваться'
-    })
-  }
-})
+app.post('/auth/login', loginValidation, UserController.login);
+app.post('/auth/register', registerValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
     if (err) {
