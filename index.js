32f0406cb890770f3ced89e53a2efc05068b08d5
Pavel Demidovich <p.demidovich@softmax.by>
2022-06-22 00:51:26 +0300

    controllers

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { registerValidation, loginValidation } from './validations/validations.j
 
 import checkAuth from './utils/checkAuth.js'
 import * as UserController from './Controllers/UserController.js'
+import * as PostController from './Controllers/PostController.js'
+
 
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.agofg.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
@@ -21,6 +23,12 @@ app.use(express.json())
 app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+// app.get('/posts', PostController.getAll);
+// app.get('/posts/:id', PostController.getOne);
+// app.post('/posts', PostController.create)
+// app.delete('/posts', PostController.remove)
+// app.patch('/posts', PostController.update)
 
 app.listen(4444, (err) => {
     if (err) {
