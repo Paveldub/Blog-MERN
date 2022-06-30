ffc4756afe01696e4e60c801979dc731e1fe8658
Pavel Demidovich <p.demidovich@softmax.by>
2022-06-30 15:29:34 +0300

    tags endpoint

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 
 // articles
 app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
 app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
